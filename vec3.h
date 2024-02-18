8deeff7a81c94f422ab019365231566f89ddda4d
Charlotte Cullip <charlotte.cullip@gmail.com>
2024-02-18 21:13:27 +0100

    Up through section 4 (horizontal blue gradient)

--- vec3.h
+++ vec3.h
@@ -8,7 +8,7 @@ using std::sqrt;
 
 class vec3 {
     public:
-        double e[3]
+        double e[3];
 
         vec3() : e{0,0,0} {}
         vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
@@ -104,7 +104,7 @@ inline double dot(const vec3 &u, const vec3 &v) {
 }
 
 // cross product of two vectors
-inline cross(const vec3 &u, const vec3 &v) {
+inline vec3 cross(const vec3 &u, const vec3 &v) {
     return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
                 u.e[2] * v.e[0] - u.e[0] * v.e[2],
                 u.e[0] * v.e[1] - u.e[1] * v.e[0]);
